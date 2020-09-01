8338b133ad1ea522c38332ccf433ff2ede1f9b9a
daibiao <db_work@126.com>
2020-09-01 15:39:23 +0800

    单链表

---
+++ src/com/datastruct/link/LinkList.java
@@ -0,0 +1,119 @@
+package com.datastruct.link;
+
+public class LinkList {
+    private int size;
+    private Node first;
+    private Node last;
+
+    public boolean add(int val) {
+        linkLast(val);
+        return true;
+    }
+
+    public boolean add(int index, int val) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("index:" + index + " size:" + size);
+        }
+        Node newNode = new Node(val, null);
+        if (index == 0) {
+            newNode.next = first;
+            first = newNode;
+            size++;
+            return true;
+        }
+        if (index == size) {
+            linkLast(val);
+            return true;
+        }
+        Node temp = first;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
+        return true;
+    }
+
+    public int remove(int index) {
+        checkBound(index);
+        int oldVal;
+        if (index == 0) {
+            oldVal = first.item;
+            first = first.next;
+            size--;
+            return oldVal;
+        }
+        Node temp = first;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        Node rNode = temp.next;
+        oldVal = rNode.item;
+        temp.next = rNode.next;
+        size--;
+        return oldVal;
+    }
+
+    private void checkBound(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index:" + index + " size:" + size);
+        }
+    }
+
+    public int get(int index) {
+        checkBound(index);
+        Node temp = first;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.item;
+    }
+
+    void linkLast(int val) {
+        final Node l = last;
+        final Node newNode = new Node(val, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        if (first == null) {
+            return "[]";
+        }
+        if (first == last) {
+            return "[" + first.item + "]";
+        }
+        StringBuilder sb = new StringBuilder("[");
+        Node temp = first;
+        while (true) {
+            sb.append(temp.item);
+            if (temp.next == null) {
+                return sb.append(']').toString();
+            }
+            sb.append(',');
+            temp = temp.next;
+        }
+    }
+
+    private static class Node {
+        private int item;
+        private Node next;
+
+        public Node(int item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+}
---
+++ src/com/datastruct/link/LinkListTest.java
@@ -0,0 +1,20 @@
+package com.datastruct.link;
+
+public class LinkListTest {
+    public static void main(String[] args) {
+        LinkList linkList = new LinkList();
+        linkList.add(1);
+        linkList.add(3);
+        System.out.println(linkList.size());
+        System.out.println(linkList);
+        linkList.add(2, 2);
+        System.out.println(linkList.size());
+        System.out.println(linkList);
+
+        linkList.remove(0);
+        System.out.println(linkList.size());
+        System.out.println(linkList);
+
+        System.out.println(linkList.get(0));
+    }
+}
