03a2802d59d32cc5ac907385f0e2288d7223cae1
daibiao <daib@dataadt.com>
2020-10-23 14:21:51 +0800

    快速排序

--- src/com/book/sort/QuickSort.java
+++ src/com/book/sort/QuickSort.java
@@ -1,19 +1,45 @@
 package com.book.sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] args) {
-
+        int[] arr = new int[]{3, 1, 4, 5, 6, 2};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static int Partition(int[] arr, int low, int high) {
-        int p = arr[0];
+    private static int partition(int[] arr, int low, int high) {
+        int p = arr[low];
         while (low < high) {
-
-            while (low<high && arr[high]>p){
-
+            while (low < high && arr[high] > p) {
+                high--;
             }
+            arr[low] = arr[high];
+
+            while (low < high && arr[low] < p) {
+                low++;
+            }
+            arr[high] = arr[low];
         }
-        return 0;
+        arr[low] = p;
+        return low;
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int index = partition(arr, low, high);
+
+            quickSort(arr, low, index - 1);
+
+            quickSort(arr, index + 1, high);
+        }
+    }
+
+    private static void swapItem(int[] arr, int low, int high) {
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
     }
 
 }
